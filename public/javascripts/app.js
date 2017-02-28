b232a1ccf55c3b03d009007f1ea0d8de83b46cb9
Elana Kopelevich <ekopelevich@gmail.com>
2017-02-28 10:22:27 -0700

    style tweaks

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -11,29 +11,38 @@ function getUsers(){
 }
 
 function showUserInfo(data, tabletop) {
-  console.log(data)
   createCards(data['final data'].elements)
 }
 
 function createCards(data) {
-
-    data.forEach(user => {
-      if (user.learning) {
-        console.log(user)
-        const card = `
-        <div class="card small-12 large-3 columns mp-card">
-          <div class="card-divider mp-card-divider">
-            ${ user.first_name } ${ user.last_name }
+  var mentorshipRoleClass
+  data.sort((a,b) =>  {
+    if (a.last_name < b.last_name) return -1
+    if (a.last_name > b.last_name) return 1
+    return 0
+  })
+  data.forEach(user => {
+    if (user.mentorship_role === 'student') {
+      mentorshipRoleClass = 'mp-student-header'
+    } else {
+      mentorshipRoleClass = 'mp-mentor-header'
+    }
+    if (user.learning) {
+      const card = `
+      <div class="card small-12 large-3 columns mp-card">
+        <div class="card-divider mp-card-header ${mentorshipRoleClass}">
+          ${ user.first_name } ${ user.last_name }
+        </div>
+        <div class="card-section">
+          <img src="${ user.photo }" alt="${ user.name }">
+            <small>Currently learning about:</small>
+            <p>${ user.learning }</p>
           </div>
-          <div class="card-section">
-            <img src="${ user.photo }" alt="${ user.name }">
-              <p>${ user.learning }</p>
-            </div>
-          </div>
-          `
-          appendCard(card)
-        }
-      })
+        </div>
+        `
+        appendCard(card)
+    }
+  })
 }
 
 function appendCard(card){
